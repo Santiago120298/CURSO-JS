115dcc4fb7e399e5d7cd6f04f5b6ae9c06b727b7
Santiago Chaves <santodechaves@hotmail.com>
2024-04-28 22:02:47 -0500

    curso_js_28_abril_2024

---
+++ 01-tipos/02-primitivos.js
@@ -0,0 +1,12 @@
+// VER IMAGENES EN INDEX 
+let numero = 1; //NUMBER
+let text = 'santiago'; //STRING
+
+let verdadero = true; //BOOLEAN
+let falso = false; //BOOLEAN
+
+let nodefinido; //INDEFINIDO = TIPO DE DATO QUE TENDRA VARIABLE QUE NO SE LE ASIGNARA UN VALOR
+let undef = undefined; //INDEFINIDO
+
+//VER IMAGEN EN INDEX PARA USAR NULL
+let nulo = null; //NULO 
