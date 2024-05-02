d18bb5eb62fa8dbf5b409b86b02d4d01e76c8224
Santiago Chaves <santodechaves@hotmail.com>
2024-05-01 21:36:18 -0500

    tercero commit JS

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,17 @@
+let a = 5; // declaration
+let b = 7;
+console.log(a + b, 'adicion'); //expresion y adicion
+console.log(a - b, 'resta'); //expresion y resta
+console.log(a * b, 'multiplicacion'); //expresion y multiplicacon
+console.log(a / b, 'division'); //expresion y division
+console.log(a % b, 'modulo o resto'); //expresion y modulo
+console.log(a ** b, 'potencia'); //expresion y potencia
+
+//incrementar
+console.log(++a);//incrementar en 1 antes de la variable
+console.log(a++);//iencrementar en 1 despues de la variable
+console.log(a);//muestra el valor 
+//decrementar
+console.log(--a);//decrementar en 1 antes de la variable
+console.log(a--);//decrementar en 1 despues de la variable
+console.log(a);//muestra el valor
---
+++ 02-operadores/02-asignacion.js
@@ -0,0 +1,10 @@
+let a =5;
+a = a + 5;
+
+a += 5;
+a -= 5;
+a *= 5;     //FORMAS PARA ASIGNAR UN VALOR ESPECIFICO A UNA VARIABLE
+a /= 5;
+a %= 5;
+a **= 5;
+console.log(a);
