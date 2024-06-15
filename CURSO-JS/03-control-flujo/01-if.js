3cb40044ad3e810ab409226c04a1edf29ff7bfe1
Santiago Chaves Martinez <santodechaves@hotmail.com>
2024-06-15 16:02:41 -0500

    Delete CURSO-JS directory

--- CURSO-JS/03-control-flujo/01-if.js
+++
@@ -1,7 +0,0 @@
-// variable igual a 25
-let edad = 25 ;
-
-// condicion que hace cumplir si el usuario es 'mayor de edad'  en este caso IF
-if (edad > 17){
-    console.log ('usuario mayor de edad' );
-}
